c4166d6037471af2e9dd92ccae1f9158a6c2abd3
kampit ojha <97059622+Itskampitojha@users.noreply.github.com>
2026-01-06 10:24:01 +0530

    Add practice.tsx file

---
+++ coding-practice/practice.tsx
@@ -0,0 +1,20 @@
+// Practice File - TypeScript + React
+
+/*
+  This is a clean practice file with no errors.
+  You can start coding here!
+*/
+
+import React from 'react';
+
+// Example: Basic React Component
+function PracticeComponent() {
+  return (
+    <div>
+      <h1>Practice Component</h1>
+      <p>Start coding here!</p>
+    </div>
+  );
+}
+
+export default PracticeComponent;
